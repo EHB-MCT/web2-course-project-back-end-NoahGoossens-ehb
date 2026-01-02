a53a281fa5aefb67d8393a79162939de9fa62a2c
Noah Goossens <noah.goossens@student.ehb.be>
2026-01-02 20:23:14 +0100

    env and database.js creation WIP

---
+++ database.js
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
+
+const mongoUri = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME || 'golfdb';
+
+if (!mongoUri) {
+  throw new Error('Missing MONGO_URI environment variable');
+}
+
+let client;
+let db;
+let connectionPromise;
+
+export async function connectDB() {
+  if (db) return db;
+
+  if (!connectionPromise) {
+    client = new MongoClient(mongoUri);
+    connectionPromise = client.connect();
+  }
+
+  await connectionPromise;
+  db = client.db(dbName);
+
+  console.log('Connected to :', db.databaseName);
+  return db;
+}
+
+export function getDB() {
+  if (!db) {
+    throw new Error('Database not connected yet. Call connectDB() first.');
+  }
+  return db;
+}
+
+//https://www.mongodb.com/docs/drivers/node/current/connect/mongoclient/
+//https://www.geeksforgeeks.org/mongodb/how-to-connect-mongodb-database-using-node-js/
